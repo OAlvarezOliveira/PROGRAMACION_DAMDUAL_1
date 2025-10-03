a46e5aeccdf2a3a49aef62e8a2b89243639ed0fb
O.Alvarez <O.Alvarez@A32PC15.acarballeira.local>
2025-10-03 21:23:59 +0200

    pruebas bucles

---
+++ src/pruebas/bucle.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package pruebas;
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * Resolucion de la tarea 
+ * 
+ * Curso 2025-26
+ * 
+ * @author O.Alvarez
+ * @version 1.0
+ *
+ */
+
+public class bucle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		/*
+		 * int contador; int num; num = 7; contador=0;
+		 * 
+		 * while(contador <= 10){ System.out.println(num+" x " + contador + "=\t" +
+		 * (num*contador)); contador++; }
+		 * 
+		 * for (int i = 0 ; i<=10;i++ ) { System.out.println(i);
+		 * 
+		 * }
+		 */
+		Scanner teclado = new Scanner(System.in);
+		char letra;
+		System.out.println("Introduce opcion:");
+		letra = teclado.next().charAt(0);
+		while (letra != 'x') {
+			System.out.println("Hola");
+			System.out.print("Introduce opcion:");
+			letra = teclado.next().charAt(0);
+		}
+		teclado.close();
+	}
+
+}
